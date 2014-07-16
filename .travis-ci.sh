d103c9ff19eba3607d0d87bd2ce1235073acc34f
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-07-16 22:25:56 +0200

    Update travis script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,3 +1,7 @@
+echo "yes" | sudo add-apt-repository ppa:avsm/ppa
+sudo apt-get update -qq
+sudo apt-get install ocaml ocaml-native-compilers camlp4-extra opam opam
+
 ./configure --enable-tests
 make
 make test
