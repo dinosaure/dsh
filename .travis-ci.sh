a7bf80cf34b4b35bae5520a6d080f440defd9dcc
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-07-16 22:29:29 +0200

    Update again and again travis script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,6 +2,11 @@ echo "yes" | sudo add-apt-repository ppa:avsm/ppa
 sudo apt-get update -qq
 sudo apt-get install ocaml ocaml-native-compilers camlp4-extra opam opam
 
+export OPAMYES=1
+opam init
+opam install oasis ocamlfind menhir
+eval `opam config env`
+
 ./configure --enable-tests
 make
 make test
