25e6465e1829bf0cab74ba59e12b7fcec4aa2a7d
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-07-17 13:18:06 +0200

    Interpret file !

--- bin/dscheme.ml
+++ bin/dscheme.ml
@@ -3,7 +3,7 @@ let interpret expr =
   try
     let tree = Parser.single_expr Lexer.token lexbuf in
     let ty = Synthesis.eval Core.core 0 tree in
-    let rt = Interpreter.eval ~env:Core.runtime tree in
+    let rt = Interpreter.eval Core.runtime tree in
     Printf.printf ": %s\n%!" (Type.to_string ty);
     Printf.printf "> %s\n%!" (Interpreter.to_string rt);
   with
@@ -30,9 +30,47 @@ let interpret expr =
       (Interpreter.string_of_exn exn)
       (Location.to_string_of_line loc expr)
 
-let () =
-  let rec repl () =
+let rec repl () =
+  begin
     Printf.printf "> %!";
     try interpret (read_line ()); repl ()
     with End_of_file -> ()
-  in repl ()
+  end
+
+let rec file inch filename =
+  let lexbuf = Lexing.from_channel inch in
+  try
+    let tree = Parser.exprs Lexer.token lexbuf in
+    Interpreter.top Core.runtime tree
+  with
+  | Parser.Error ->
+    let loc = Location.make
+        (Lexing.lexeme_start_p lexbuf)
+        (Lexing.lexeme_end_p lexbuf)
+    in
+    Printf.printf "Parsing error at %s:\n> %s\n%!"
+      (Location.to_string loc)
+      (Location.to_string_of_file loc filename)
+  | Synthesis.Error (loc, exn) ->
+    Printf.printf "Typing error - %s at %s:\n> %s\n%!"
+      (Synthesis.string_of_exn exn)
+      (Location.to_string loc)
+      (Location.to_string_of_file loc filename)
+  | Lexer.Lexical_error ->
+    let loc = Location.make
+        (Lexing.lexeme_start_p lexbuf)
+        (Lexing.lexeme_end_p lexbuf)
+    in
+    Printf.printf "Lexical error at %s:\n> %s\n%!"
+      (Location.to_string loc)
+      (Location.to_string_of_file loc filename)
+  | Interpreter.Error (loc, exn) ->
+    Printf.printf "Interpreter error - %s at %s:\n> %s\n%!"
+      (Interpreter.string_of_exn exn)
+      (Location.to_string loc)
+      (Location.to_string_of_file loc filename)
+
+let () =
+  if Array.length Sys.argv = 2
+  then file (open_in Sys.argv.(1)) Sys.argv.(1)
+  else repl ()
