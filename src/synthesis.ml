3bcfe4bc0e658d3bc0775f1eb811e8713c73d53a
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-08-13 23:33:08 +0200

    Fix bug with alias and specialization

--- src/synthesis.ml
+++ src/synthesis.ml
@@ -405,7 +405,7 @@ let specialization level ty =
       let lst = List.rev_map (fun _ -> Variable.make level) ids in
       substitution ids lst ty
     | Type.Var { contents = Type.Link ty } -> aux level ty
-    | Type.Alias (_, ty) -> aux level ty
+    | Type.Alias (name, ty) -> Type.Alias (name, aux level ty)
     | ty -> ty
   in aux level ty
 
