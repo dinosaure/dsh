471fff09a3b2efb081547b69d5acc081e62afbb9
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-10-10 12:56:58 +0200

    Remove datatype of Gamma environment

--- src/gamma.ml
+++ src/gamma.ml
@@ -1,74 +1,16 @@
 type t =
   {
     alias    : Type.t Map.Make(String).t;
-    datatype : Type.t Map.Make(String).t;
   }
 
 module G = Map.Make(String)
 
-module Alias = struct
-  let lookup key { alias; _ } = G.find key alias
-end
-
-module Datatype = struct
-  let exists ctor { datatype; _ } =
-    let exists ctor map =
-      G.exists (fun _ -> function
-          | Type.Set l -> Type.Set.mem ctor l
-          | _ -> false)
-        map
-    in
-    exists ctor datatype
-
-  let lookup ctor { datatype; _ } =
-    let filter ctor map =
-      G.filter (fun _ -> function
-          | Type.Set l -> Type.Set.mem ctor l
-          | _ -> raise (Invalid_argument "Gamma.Datatype.lookup"))
-        map
-    in
-    G.choose (filter ctor datatype)
-
-  let find ctor { datatype; _ } =
-    let filter ctor map =
-      G.filter (fun _ -> function
-          | Type.Set l -> Type.Set.mem ctor l
-          | _ -> false)
-        map
-    in
-    try Some (G.choose (filter ctor datatype))
-    with Not_found -> None
-end
-
-exception Constructor_already_exists of string
-
-let () = Printexc.register_printer
-    (function
-      | Constructor_already_exists ctor ->
-        Some (Printf.sprintf "constructor %s already exists" ctor)
-      | _ -> None)
-
 let add key value gamma = match value with
-  | Type.Set l ->
-    let exists = Type.Set.fold
-        (fun ctor _ ->
-           function
-           | None -> if Datatype.exists ctor gamma
-             then Some ctor
-             else None
-           | x -> x)
-        l None
-    in exists |> (function
-      | Some ctor -> raise (Constructor_already_exists ctor)
-      | None -> { gamma with datatype = G.add key value gamma.datatype })
-  | _ -> { gamma with alias = G.add key value gamma.alias }
+  | _ -> { alias = G.add key value gamma.alias }
 
-let lookup key { alias; datatype; } =
-  try G.find key alias
-  with Not_found -> G.find key datatype
+let lookup key { alias; } = G.find key alias
 
 let empty =
   {
     alias = G.empty;
-    datatype = G.empty;
   }
--- src/gamma.mli
+++ src/gamma.mli
@@ -1,17 +1,5 @@
 type t
 
-module Alias : sig
-  val lookup : string -> t -> Type.t
-end
-
-module Datatype : sig
-  val exists : string -> t -> bool
-  val lookup : string -> t -> (string * Type.t)
-  val find : string -> t -> (string * Type.t) option
-end
-
-exception Constructor_already_exists of string
-
 val add : string -> Type.t -> t -> t
 val lookup : string -> t -> Type.t
 val empty : t
