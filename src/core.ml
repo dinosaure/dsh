8d3fefd00e23919692b9787637477f8f3cb6d9d5
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-11-11 09:01:19 +0100

    Update syntax and describe an error

--- src/core.ml
+++ src/core.ml
@@ -1,62 +1,77 @@
 let add expr env =
-  let expr = Parser.single_expr Lexer.token (Lexing.from_string expr) in
-  match expr with
-  | Ast.Ann (_, Ast.Var (_, name), ([], ty)) ->
-    Synthesis.Environment.add name ty env
-  | _ -> raise (Invalid_argument ("Core.add: " ^ (Ast.to_string expr)))
+  let lexbuf = Sedlexing.Utf8.from_string expr in
+  let token, start, stop = Ulexer.parse () in
+  try
+    let expr = Uparser.single_expr token lexbuf in
+    match expr with
+    | Ast.Ann (_, Ast.Var (_, name), ([], ty)) ->
+      Synthesis.Environment.add name ty env
+    | _ -> raise (Invalid_argument ("Core.add: " ^ (Ast.to_string expr)))
+  with
+  | Uparser.Error ->
+    let loc = Location.make
+        (start lexbuf)
+        (stop lexbuf)
+    in Printf.printf "Parsing error at:\n%s\n%!"
+      (Location.to_string_of_line loc expr); env
+  | Ulexer.Lexical_error ->
+    let loc = Location.make
+        (start lexbuf)
+        (stop lexbuf)
+    in Printf.printf "Lexical error at:\n%s%!"
+      (Location.to_string_of_line loc expr); env
 
 let core =
   Synthesis.Environment.empty
-  |> add "id      : (V (a) (a > a))"
-  |> add "choose  : (V (a) (a > a > a))"
-  |> add "apply   : (V (a b) ((a > b) > a > b))"
-  |> add "const   : (V (a b) (a > b > a))"
+  |> add "id      : ∀a. (a → a)"
+  |> add "choose  : ∀a. (a → a → a)"
+  |> add "apply   : ∀a b. ((a → b) → a → b)"
+  |> add "const   : ∀a b. (a → b → a)"
 
-  |> add "head    : (V (a) ((list a) > a))"
-  |> add "tail    : (V (a) ((list a) > (list a)))"
-  |> add "nill    : (V (a) (list a))"
-  |> add "cons    : (V (a) (a > (list a) > (list a)))"
-  |> add "single  : (V (a) (a > (list a)))"
-  |> add "map     : (V (a b) ((a > b) > (list a) > (list b)))"
-  |> add "length  : (V (a) ((list a) > int))"
-  |> add "empty   : (V (a) ((list a) > bool))"
+  |> add "head    : ∀a. (list[a] → a)"
+  |> add "tail    : ∀a. (list[a] → list[a])"
+  |> add "nill    : ∀a. list[a]"
+  |> add "cons    : ∀a. (a → list[a] → list[a])"
+  |> add "single  : ∀a. (a → list[a])"
+  |> add "map     : ∀a b. ((a → b) → list[a] → list[b])"
+  |> add "length  : ∀a. (list[a] → int)"
+  |> add "empty   : ∀a. (list[a] → bool)"
 
-  |> add "succ    : (int > int)"
-  |> add "pred    : (int > int)"
-  |> add "+       : (int > int > int)"
-  |> add "-       : (int > int > int)"
-  |> add "*       : (int > int > int)"
-  |> add "/       : (int > int > int)"
-  |> add "%       : (int > int > int)"
+  |> add "succ    : (int → int)"
+  |> add "pred    : (int → int)"
+  |> add "+       : (int → int → int)"
+  |> add "-       : (int → int → int)"
+  |> add "*       : (int → int → int)"
+  |> add "/       : (int → int → int)"
+  |> add "%       : (int → int → int)"
 
-  |> add "=       : (V (a) (a > a > bool))"
-  |> add "<>      : (V (a) (a > a > bool))"
-  |> add ">       : (int > int > bool)"
-  |> add "<       : (int > int > bool)"
-  |> add ">=      : (int > int > bool)"
-  |> add "<=      : (int > int > bool)"
-  |> add "not     : (bool > bool)"
-  |> add "and     : (bool > bool > bool)"
-  |> add "or      : (bool > bool > bool)"
+  |> add "equal   : ∀a. (a → a → bool)"
+  |> add "<>      : ∀a. (a → a → bool)"
+  |> add ">       : (int → int → bool)"
+  |> add "<       : (int → int → bool)"
+  |> add ">=      : (int → int → bool)"
+  |> add "<=      : (int → int → bool)"
 
-  |> add "fst     : (V (a b) ((* a b) > a))"
-  |> add "snd     : (V (a b) ((* a b) > b))"
+  |> add "not     : (bool → bool)"
+  |> add "and     : (bool → bool → bool)"
+  |> add "or      : (bool → bool → bool)"
 
-  |> add "num     : (int > unit)"
-  |> add "bln     : (bool > unit)"
-  |> add "chr     : (char > unit)"
+  |> add "fst     : ∀a b. (tuple[a, b] → a)"
+  |> add "snd     : ∀a b. (tuple[a, b] → b)"
 
-  |> add "any     : (V (a) a)"
-  |> add "magic   : (V (a b) (a > b))"
-  |> add "poly    : ((V (a) (a > a)) > (* int bool))"
-  |> add "special : (((V (a) (a > a)) > (V (a) (a > a)))
-                     > (V (a) (a > a)))"
-  |> add "id'     : ((V (a) (a > a)) > (V (a) (a > a)))"
-  |> add "id''    : (V (a) ((V (a) (a > a)) > (a > a)))"
-  |> add "ids     : (list (V (a) (a > a)))"
-  |> add "ids'    : ((list (V (a) (a > a)))
-                     > (list (V (a) (a > a))))"
-  |> add "magid   : ((V (a b) (a > b)) > (V (a b) (a > b)))"
+  |> add "num     : (int → unit)"
+  |> add "bln     : (bool → unit)"
+  |> add "chr     : (char → unit)"
+
+  |> add "any     : ∀a. a"
+  |> add "magic   : ∀a b. (a → b)"
+  |> add "poly    : ((∀a. (a → a)) → tuple[int, bool])"
+  |> add "special : ((∀a. (a → a)) → (∀a. (a → a)) → (∀a. (a → a)))"
+  |> add "id'     : ((∀a. (a → a)) → (∀a. (a → a)))"
+  |> add "id''    : ∀a. ((∀a. (a → a)) → (a → a))"
+  |> add "ids     : list[∀a. (a → a)]"
+  |> add "ids'    : (list[∀a. (a → a)] → list[∀a. (a → a)])"
+  |> add "magid   : ((∀a b. (a → b)) → (∀a b. (a → b)))"
 
 let add name func env =
   Interpreter.Environment.add name (Interpreter.Primitive func) env
@@ -65,40 +80,3 @@ let runtime =
   let raise_error name = raise (Invalid_argument name) in
   let open Interpreter in
   Environment.empty
-  |> add "+" (function [Int a; Int b] -> Int (a + b) | _ -> raise_error "+")
-  |> add "-" (function [Int a; Int b] -> Int (a - b) | _ -> raise_error "-")
-  |> add "*" (function [Int a; Int b] -> Int (a * b) | _ -> raise_error "*")
-  |> add "/" (function [Int a; Int b] -> Int (a / b) | _ -> raise_error "/")
-  |> add "%" (function [Int a; Int b] -> Int (a mod b) | _ -> raise_error "%")
-  |> add "succ" (function [Int a] -> Int (a + 1) | _ -> raise_error "succ")
-  |> add "pred" (function [Int a] -> Int (a - 1) | _ -> raise_error "pred")
-
-  |> add "=" (function [a; b] -> Bool (a = b) | _ -> raise_error "=")
-  |> add "==" (function [a; b] -> Bool (a == b) | _ -> raise_error "=")
-  |> add "<>" (function [a; b] -> Bool (a <> b) | _ -> raise_error "=")
-  |> add "!=" (function [a; b] -> Bool (a != b) | _ -> raise_error "=")
-  |> add ">" (function [Int a; Int b] -> Bool (a > b) | _ -> raise_error ">")
-  |> add "<" (function [Int a; Int b] -> Bool (a < b) | _ -> raise_error "<")
-  |> add ">=" (function [Int a; Int b] -> Bool (a >= b) | _ -> raise_error ">=")
-  |> add "<=" (function [Int a; Int b] -> Bool (a <= b) | _ -> raise_error "<=")
-  |> add "not" (function [Bool a] -> Bool (not a) | _ -> raise_error "not")
-
-  |> add "num"
-    (function [Int a] -> print_int a; Unit
-            | _ -> raise_error "#num")
-  |> add "bln"
-    (function [Bool b] -> print_string (if b then "true" else "false"); Unit
-            | _ -> raise_error "#bln")
-  |> add "chr"
-    (function [Char c] -> print_string (String.make 1 c); Unit
-            | _ -> raise_error "#chr")
-
-  |> add "and" (function [Bool a; Bool b] -> Bool (a && b)
-                       | _ -> raise_error "and")
-  |> add "or" (function [Bool a; Bool b] -> Bool (a || b)
-                      | _ -> raise_error "or")
-
-  |> add "," (function l -> Tuple l)
-  |> add "fst" (function [Tuple l] -> List.hd l | _ -> raise_error "fst")
-  |> add "snd" (function [Tuple l] -> List.tl l |> List.hd
-                       | _ -> raise_error "snd")
