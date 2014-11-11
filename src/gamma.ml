2ae7fdbbccb1f34e390b341bd41e35576d07661e
Romain Calascibetta <romain.calascibetta@gmail.com>
2014-11-11 08:51:29 +0100

    Remove useless container of gamma environment

--- src/gamma.ml
+++ src/gamma.ml
@@ -1,16 +1,10 @@
-type t =
-  {
-    alias    : Type.t Map.Make(String).t;
-  }
+type t = Type.t Map.Make(String).t
 
 module G = Map.Make(String)
 
 let add key value gamma = match value with
-  | _ -> { alias = G.add key value gamma.alias }
+  | _ -> G.add key value gamma
 
-let lookup key { alias; } = G.find key alias
+let lookup key gamma = G.find key gamma
 
-let empty =
-  {
-    alias = G.empty;
-  }
+let empty = G.empty;
